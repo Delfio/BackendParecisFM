d586be213e8d483531e3e252271d6cc9c434727b
Delfio <delfio_eu@hotmail.com>
2020-02-02 09:21:02 -0400

    Return Main Infos

--- src/app/controllers/PrincipalController.js
+++ src/app/controllers/PrincipalController.js
@@ -1,12 +1,79 @@
 import Radio from '../models/Radio';
 import Banner from '../models/Banner';
 import Programacao from '../models/Programacao';
+import Dias from '../models/Dia';
+
+import { Op } from 'sequelize'
+
+import {
+  parseISO, 
+  isBefore,  
+  getHours,
+  isFriday, //sexta
+  isMonday,  //segunda
+  isSaturday, //sábado
+  isSunday, //Domingo
+  isThursday, //quinta
+  isTuesday, //terça
+  isWednesday //quarta
+} from 'date-fns';
 
 class PrincipalController {
 
   async index(req, res){
 
     const { id: IdRadio } = req.params;
+
+    const { data } = req.query
+
+    //Pegando a hora atual do cliente - 18
+    const isHourAtual = getHours(new Date(data));
+
+    const qtdNumeros = `${isHourAtual}`;
+    
+    // // Formatando ela para 18:00
+    const isHourFormated = () =>{
+      if(qtdNumeros.length == 1){
+        const x  = `0${isHourAtual}:00`;
+        return x;
+  
+      }else if(qtdNumeros.length == 2){
+        const x  = `${isHourAtual}:00`;
+        return x;
+  
+      }
+    }
+    const horanessaporra = isHourFormated();
+
+    // // //Verificando qual dia esta data pertence
+    const dia = () => {
+      if( isMonday(parseISO(data))){
+        return "Segunda-Feira";
+        
+      }else if(isTuesday(parseISO(data))){
+        return "Terça-Feira";
+
+      }else if(isWednesday(parseISO(data))){
+        return "Quarta-Feira";
+
+      }else if(isThursday(parseISO(data))){
+        return "Quinta-Feira";
+
+      }else if(isFriday(parseISO(data))){
+        return "Sexta-Feita";
+
+      }else if(isSaturday(parseISO(data))){
+        return "Sabado";
+        
+      }else if(isSunday(parseISO(data))){
+        return "Domingo";
+        
+      }
+    }
+    //executando a função e pegando seu valor
+    const diaAtual = dia();
+
+
 
     //Banners e infos da rádio
     const radio = await Radio.findAll({
@@ -29,9 +96,30 @@ class PrincipalController {
           order:[
             ['id', 'DESC']
           ],
+        },
+        {
+          model: Programacao,
+          as: 'programacao',
+          limit: 1,
+          where:{ 
+            horario: {
+              [Op.like]: `%${horanessaporra}%`
+            }
+          },
+          include: [
+            {
+              model: Dias,
+              as: 'dia',
+              where: { 
+                nome: diaAtual
+               }
+            }
+          ]
         }
       ]
-    })
+    });
+
+
 
     return res.json(radio)
   }
