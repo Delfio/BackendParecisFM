9c2dc86be0ce4ce0c3e19ddf9faacf04fd0d967b
Delfio <delfio_eu@hotmail.com>
2020-03-10 19:53:36 -0400

    Time format service for broken hours

--- src/app/controllers/PrincipalController.js
+++ src/app/controllers/PrincipalController.js
@@ -1,208 +1,56 @@
-import Radio from '../models/Radio';
-import Banner from '../models/Banner';
-import Programacao from '../models/Programacao';
-import Programa from '../models/Programa';
-import Dias from '../models/Dia';
-import User from '../models/User';
-import Avatar from '../models/FotoLocutor';
-import Contato from '../models/Contato';
-import Top3 from '../models/Top3';
-import ImagemTop3s from '../models/ImagemTop3';
-import Contato from '../models/Contato';
-import Dia from '../models/Dia';
-import Cidade from '../models/Cidade';
-import Icon from '../models/IconRadio';
+import Radio from "../models/Radio";
+import Banner from "../models/Banner";
+import Programacao from "../models/Programacao";
+import Programa from "../models/Programa";
+import Dias from "../models/Dia";
+import User from "../models/User";
+import Avatar from "../models/FotoLocutor";
+import Contato from "../models/Contato";
+import Top3 from "../models/Top3";
+import ImagemTop3s from "../models/ImagemTop3";
+import Dia from "../models/Dia";
+import Cidade from "../models/Cidade";
+import Icon from "../models/IconRadio";
 
-import { Op } from 'sequelize'
+import { Op } from "sequelize";
 
 import {
-  parseISO, 
-  isBefore,  
+  parseISO,
+  isBefore,
+  getMinutes,
   getHours,
   isFriday, //sexta
-  isMonday,  //segunda
+  isMonday, //segunda
   isSaturday, //sábado
   isSunday, //Domingo
   isThursday, //quinta
   isTuesday, //terça
   isWednesday //quarta
-} from 'date-fns';
+} from "date-fns";
+
+import ServiceFormateHour from "../../services/FormatHourAndGetInformation";
 
 class PrincipalController {
-
-  async index(req, res){
+  async index(req, res) {
     try {
-      
-    const { id: IdRadio } = req.params;
+      const { id: IdRadio } = req.params;
 
-    const { data } = req.query
+      const { data } = req.query;
 
-    // if( isBefore(parseISO(data), new Date())){
-    //   return res.status(400).json({error: 'Data Anterior a atual'})
-    // }
+      // if( isBefore(parseISO(data), new Date())){
+      //   return res.status(400).json({error: 'Data Anterior a atual'})
+      // }
 
-    //Pegando a hora atual do cliente - 18
-    const isHourAtual = getHours(new Date(data));
+      const radio = await ServiceFormateHour.run({
+        date: data,
+        radio_id: IdRadio
+      });
 
-    const qtdNumeros = `${isHourAtual}`;
-    
-    // // Formatando ela para 18:00
-    const isHourFormated = () =>{
-      if(qtdNumeros.length == 1){
-        const x  = `0${isHourAtual}:00`;
-        return x;
-  
-      }else if(qtdNumeros.length == 2){
-        const x  = `${isHourAtual}:00`;
-        return x;
-  
-      }
-    }
-    const horanessaporra = isHourFormated();
-
-    //Verificando qual dia esta data pertence
-    const dia = () => {
-      if( isMonday(parseISO(data))){
-        return "Segunda-Feira";
-        
-      }else if(isTuesday(parseISO(data))){
-        return "Terça-Feira";
-
-      }else if(isWednesday(parseISO(data))){
-        return "Quarta-Feira";
-
-      }else if(isThursday(parseISO(data))){
-        return "Quinta-Feira";
-
-      }else if(isFriday(parseISO(data))){
-        return "Sexta-Feira";
-
-      }else if(isSaturday(parseISO(data))){
-        return "Sabado";
-        
-      }else if(isSunday(parseISO(data))){
-        return "Domingo";
-        
-      }
-    }
-    //executando a função e pegando seu valor
-    const diaAtual = dia();
-
-    //Pegar o id do dia atual
-    const {id} = await Dias.findOne({
-      where: {nome: diaAtual}
-    })
-    // const programcaoTnc = await Programacao.findAll({
-    //   where:{ 
-    //     dia_id: id,
-    //     horario: {
-    //       [Op.like]: `%${String(horanessaporra)}%`
-    //     },
-    //   }
-    // })
-    // console.log( "sdfasdfsdf :  " ,programcaoTnc, "  sadfsdfsdf ....")
-
-
-    //Banners e infos da rádio
-    const radio = await Radio.findAll({
-      where:{ id: IdRadio },
-      include: [
-        {
-          model: Banner,
-          as: 'banner1',
-          where:{ type: 1 },
-          limit: 1,
-          order:[
-            ['id', 'DESC']
-          ],
-        },
-        {
-          model: Banner,
-          as: 'banner2',
-          where:{ type: 2 },
-          limit: 1,
-          order:[
-            ['id', 'DESC']
-          ],
-        },
-        {
-          model: Programacao,
-          as: 'programacao',
-          limit: 1,
-          where:{ 
-            horario: {
-              [Op.like]: `%${horanessaporra}%`
-            },
-            dia_id: id
-          },
-          include: [
-            {
-              model: User,
-              as: 'locutor',
-              attributes: [ 'id' ,'name', 'email' ],
-              include: [
-                {
-                  model: Avatar,
-                  as: 'avatar'
-                }
-              ]
-            },
-            {
-              model: Programa,
-              as: 'programa'
-            }
-          ]
-        },
-        {
-          model: Top3,
-          as: 'top3',
-          limit: 3,
-          order:[
-            ['id', 'DESC']
-          ],
-          include:[
-            {
-              model: ImagemTop3s,
-              as: 'image'
-            }
-          ]
-        },
-        {
-          model: Programacao,
-          as: 'allprogramacao',
-          include: [
-            {
-              model: Dia,
-              as: 'dia'
-            }
-          ]
-        },
-        {
-          model: Contato,
-          as: 'contato',
-          limit: 3,
-          order:[
-            ['id', 'DESC']
-          ]
-        },
-        {
-          model: Cidade,
-          as: 'cidade',
-          attributes: ['nome']
-        },
-        {
-          model: Icon,
-          as: 'icon'
-        }
-      ]
-    });
-
-    return res.json(radio)
-    } catch(err){
-      return res.json({error: err.message})
+      return res.json(radio);
+    } catch (err) {
+      return res.json({ error: err.message });
     }
   }
-
 }
 
 export default new PrincipalController();
---
+++ src/services/FormatHourAndGetInformation.js
@@ -0,0 +1,291 @@
+import Dias from "../app/models/Dia";
+import Programacao from "../app/models/Programacao";
+import Radio from "../app/models/Radio";
+import Banner from "../app/models/Banner";
+import Programacao from "../app/models/Programacao";
+import Programa from "../app/models/Programa";
+import Dias from "../app/models/Dia";
+import User from "../app/models/User";
+import Avatar from "../app/models/FotoLocutor";
+import Contato from "../app/models/Contato";
+import Top3 from "../app/models/Top3";
+import ImagemTop3s from "../app/models/ImagemTop3";
+import Contato from "../app/models/Contato";
+import Dia from "../app/models/Dia";
+import Cidade from "../app/models/Cidade";
+import Icon from "../app/models/IconRadio";
+
+import { Op } from "sequelize";
+import {
+  parseISO,
+  isBefore,
+  getMinutes,
+  getHours,
+  isFriday, //sexta
+  isMonday, //segunda
+  isSaturday, //sábado
+  isSunday, //Domingo
+  isThursday, //quinta
+  isTuesday, //terça
+  isWednesday //quarta
+} from "date-fns";
+
+class FormatedHourAndGetInformation {
+  async run({ date, radio_id }) {
+    try {
+      //Pegando o minuto atual do cliente - 45
+      const isMinuteAtual = getMinutes(new Date(date));
+      const isHourAtual = getHours(new Date(date));
+
+      const hours = `${isHourAtual}`; //Variavel para avaliar as horas
+      const minuted = `${isMinuteAtual}`; //Variavel para avaliar os minutos
+
+      // Formatando a hora para duas casas - caso seja necessárip
+      const isHourFormated = () => {
+        if (hours.length == 1) {
+          const x = `0${isHourAtual}`;
+          return x;
+        } else if (hours.length == 2) {
+          return isHourAtual;
+        }
+      };
+      //Pegando o valor da hora formatada
+      const hourFormateds = isHourFormated();
+
+      // Formatando os minutos para 2 casas
+      const isMinutedFormats = () => {
+        if (minuted.length == 1) {
+          const x = `0${isMinuteAtual}`;
+          return x;
+        } else {
+          return isMinuteAtual;
+        }
+      };
+      const minFormateds = isMinutedFormats();
+
+      //Verificando qual dia esta data pertence
+      const dia = () => {
+        if (isMonday(parseISO(date))) {
+          return "Segunda-Feira";
+        } else if (isTuesday(parseISO(date))) {
+          return "Terça-Feira";
+        } else if (isWednesday(parseISO(date))) {
+          return "Quarta-Feira";
+        } else if (isThursday(parseISO(date))) {
+          return "Quinta-Feira";
+        } else if (isFriday(parseISO(date))) {
+          return "Sexta-Feira";
+        } else if (isSaturday(parseISO(date))) {
+          return "Sabado";
+        } else if (isSunday(parseISO(date))) {
+          return "Domingo";
+        }
+      };
+      //executando a função e pegando seu valor
+      const diaAtual = dia();
+
+      //Pegar o id do dia atual
+      const { id } = await Dias.findOne({
+        where: { nome: diaAtual }
+      });
+
+      // Hora formatada 18:45 18:00 ...
+      const hourFormated = `${hourFormateds}:${minFormateds}`;
+
+      console.log(hourFormated);
+
+      // Buscar por programação quebrada
+      const programacaoQuebrada = await Programacao.findOne({
+        where: {
+          horario: {
+            [Op.like]: `%${hourFormated}%`
+          },
+          dia_id: id
+        }
+      });
+
+      const ProgramacaoId = programacaoQuebrada ? programacaoQuebrada.id : null;
+
+      if (ProgramacaoId) {
+        //Banners e infos da rádio
+        const radio = await Radio.findAll({
+          where: { id: radio_id },
+          include: [
+            {
+              model: Banner,
+              as: "banner1",
+              where: { type: 1 },
+              limit: 1,
+              order: [["id", "DESC"]]
+            },
+            {
+              model: Banner,
+              as: "banner2",
+              where: { type: 2 },
+              limit: 1,
+              order: [["id", "DESC"]]
+            },
+            {
+              model: Programacao,
+              as: "programacao",
+              limit: 1,
+              where: {
+                id: ProgramacaoId,
+                dia_id: id
+              },
+              include: [
+                {
+                  model: User,
+                  as: "locutor",
+                  attributes: ["id", "name", "email"],
+                  include: [
+                    {
+                      model: Avatar,
+                      as: "avatar"
+                    }
+                  ]
+                },
+                {
+                  model: Programa,
+                  as: "programa"
+                }
+              ]
+            },
+            {
+              model: Top3,
+              as: "top3",
+              limit: 3,
+              order: [["id", "DESC"]],
+              include: [
+                {
+                  model: ImagemTop3s,
+                  as: "image"
+                }
+              ]
+            },
+            {
+              model: Programacao,
+              as: "allprogramacao",
+              include: [
+                {
+                  model: Dia,
+                  as: "dia"
+                }
+              ]
+            },
+            {
+              model: Contato,
+              as: "contato",
+              limit: 3,
+              order: [["id", "DESC"]]
+            },
+            {
+              model: Cidade,
+              as: "cidade",
+              attributes: ["nome"]
+            },
+            {
+              model: Icon,
+              as: "icon"
+            }
+          ]
+        });
+
+        return radio;
+      } else {
+        const hourFormated2 = `${hourFormateds}:00`;
+        //Banners e infos da rádio
+        const radio = await Radio.findAll({
+          where: { id: radio_id },
+          include: [
+            {
+              model: Banner,
+              as: "banner1",
+              where: { type: 1 },
+              limit: 1,
+              order: [["id", "DESC"]]
+            },
+            {
+              model: Banner,
+              as: "banner2",
+              where: { type: 2 },
+              limit: 1,
+              order: [["id", "DESC"]]
+            },
+            {
+              model: Programacao,
+              as: "programacao",
+              limit: 1,
+              where: {
+                horario: {
+                  [Op.like]: `%${hourFormated2}%`
+                },
+                dia_id: id
+              },
+              include: [
+                {
+                  model: User,
+                  as: "locutor",
+                  attributes: ["id", "name", "email"],
+                  include: [
+                    {
+                      model: Avatar,
+                      as: "avatar"
+                    }
+                  ]
+                },
+                {
+                  model: Programa,
+                  as: "programa"
+                }
+              ]
+            },
+            {
+              model: Top3,
+              as: "top3",
+              limit: 3,
+              order: [["id", "DESC"]],
+              include: [
+                {
+                  model: ImagemTop3s,
+                  as: "image"
+                }
+              ]
+            },
+            {
+              model: Programacao,
+              as: "allprogramacao",
+              include: [
+                {
+                  model: Dia,
+                  as: "dia"
+                }
+              ]
+            },
+            {
+              model: Contato,
+              as: "contato",
+              limit: 3,
+              order: [["id", "DESC"]]
+            },
+            {
+              model: Cidade,
+              as: "cidade",
+              attributes: ["nome"]
+            },
+            {
+              model: Icon,
+              as: "icon"
+            }
+          ]
+        });
+
+        return radio;
+      }
+    } catch (err) {
+      return err.message;
+    }
+  }
+}
+
+export default new FormatedHourAndGetInformation();
