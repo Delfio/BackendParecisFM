06715aaca08be8b17b7f2b70881b52609437106b
Delfio <delfio_eu@hotmail.com>
2020-03-10 19:54:00 -0400

    clean crontroll

--- src/app/controllers/PrincipalController.js
+++ src/app/controllers/PrincipalController.js
@@ -1,33 +1,3 @@
-import Radio from "../models/Radio";
-import Banner from "../models/Banner";
-import Programacao from "../models/Programacao";
-import Programa from "../models/Programa";
-import Dias from "../models/Dia";
-import User from "../models/User";
-import Avatar from "../models/FotoLocutor";
-import Contato from "../models/Contato";
-import Top3 from "../models/Top3";
-import ImagemTop3s from "../models/ImagemTop3";
-import Dia from "../models/Dia";
-import Cidade from "../models/Cidade";
-import Icon from "../models/IconRadio";
-
-import { Op } from "sequelize";
-
-import {
-  parseISO,
-  isBefore,
-  getMinutes,
-  getHours,
-  isFriday, //sexta
-  isMonday, //segunda
-  isSaturday, //sábado
-  isSunday, //Domingo
-  isThursday, //quinta
-  isTuesday, //terça
-  isWednesday //quarta
-} from "date-fns";
-
 import ServiceFormateHour from "../../services/FormatHourAndGetInformation";
 
 class PrincipalController {
